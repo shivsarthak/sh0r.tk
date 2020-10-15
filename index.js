932f58babc5ab126c2596d9ce98d8142cbbe34d1
shivsarthak <shivsarthak34@gmail.com>
2020-10-16 00:36:39 +0530

    final push for Heroku build

--- index.js
+++ index.js
@@ -1,21 +1,55 @@
 const express = require('express');
-
 const app = express();
+const url = process.env.DB_URL;
+const db = require('monk')(url);
 
-app.get('/', (req, res) => {
 
-    res.send(`<h1>Url Shortener<h1>`);
+function randomStr(len) {
+    return Math.random().toString(36).slice(2, len + 2);
+}
 
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('./public'));
+
+app.post('/', async(req, res, next) => {
+    try {
+        var id = req.body.tag;
+        console.log(id == "");
+        if (id == "") {
+            id = randomStr(4);
+        }
+        const collection = db.get('urlDB');
+        const result = await collection.findOne({ tag: id });
+        if (result) {
+            throw new Error("Tag already Exists");
+        }
+        const write = await collection.insert({ tag: id, url: req.body.url });
+        res.send(`<h3>Url is : <a href="http://sh0r.tk/${write.tag}">http://sh0r.tk/${write.tag}</a></h3>`, 200);
+        client.close();
+
+    } catch (err) {
+        next(err.message);
+    }
 });
 
-app.get('/:id', (req, res) => {
-
-    res.json({
-        url: `req.params.id`
-    });
-
+app.get('/:id', async(req, res) => {
+    try {
+        const collection = db.get('urlDB');
+        var result = await collection.findOne({ tag: req.params.id });
+        if (result)
+            res.redirect(result.url);
+        else {
+            res.status(404).send("NOT FOUND");
+        }
+    } catch (err) {
+        console.log(err);
+        res.send("Error Occured try again", 500);
+    }
 });
 
-app.listen(3000, function () {
+
+
+app.listen(3000, function() {
     console.log('Server is listening on port 8080')
 });
